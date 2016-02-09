d99fb71ae926afc45c26f513fd890dca594ea4c3
adarsh0608 <adarsh108@gmail.com>
2016-02-08 18:44:37 -0800

    changed secret

--- blog.py
+++ blog.py
@@ -15,7 +15,7 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-secret = 'art'
+secret = 'adarsh'
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
